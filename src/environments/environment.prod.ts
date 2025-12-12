7b1318cac446488146f55a527f24a41f90d5f4b1
Iván Betanzos Macías <ivan.betanzos89@gmail.com>
2025-12-12 10:14:15 +0100

    Actualizar environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ai-games-price-search-backend.onrender.com/api'
+  apiUrl: 'https://ai-games-price-search-backend.vercel.app/api'
 };
