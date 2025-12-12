f26935153a92988fdf93c074760c63815070b795
iv4n89 <ivan.betanzos89@gmail.com>
2025-12-12 03:52:45 +0100

    fix: added envs

--- src/app/gamescanner.service.ts
+++ src/app/gamescanner.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 export interface GameCandidate {
   title: string;
@@ -45,7 +46,7 @@ export interface AppraisalResponse {
   providedIn: 'root'
 })
 export class GameService {
-  private apiUrl = 'http://localhost:3000/api'; 
+  private apiUrl = environment.apiUrl; 
 
   constructor(private http: HttpClient) {}
 
